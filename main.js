629d0e23ca582b97bbe56b527696b194ca99696f
sushant-kunwar <112830659+sushant-kunwar@users.noreply.github.com>
2024-04-05 19:37:07 -0400

    added whipser code from class

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 var socket = new WebSocket("ws://cpsc484-04.stdusr.yale.internal:8888/frames");
+
+
+var whisper = {
+    socket: null,
+
+    start: function() {
+        var url = "ws://cpsc484-04.stdusr.yale.internal:8888/frames" + host + "sp2tx";
+        whisper.socket = new WebSocket(url);
+        whisper.socket.onmessage = function (event) {
+            whisper.show(event.data);
+        }
+    },
+    show: function (sp2tx) {
+        console.log(sp2tx)
+    }
+}
