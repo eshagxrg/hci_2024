ecfeb333dbeb91ca8c8e26bc195af07dbbae83e0
sushant-kunwar <112830659+sushant-kunwar@users.noreply.github.com>
2024-04-09 16:06:14 -0400

    Update main.js
    functioning speech to text as well as methodology to witch between html pages

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
-var socket = new WebSocket("ws://cpsc484-04.stdusr.yale.internal:8888/frames");
+var host = "cpsc484-04.stdusr.yale.internal:8888";
+$(document).ready(function() {
+  sp2tx.start();
+});
 
+var sp2tx = {
+  socket: null,
 
-var whisper = {
-    socket: null,
-
-    start: function() {
-        var url = "ws://cpsc484-04.stdusr.yale.internal:8888/frames" + host + "sp2tx";
-        whisper.socket = new WebSocket(url);
-        whisper.socket.onmessage = function (event) {
-            whisper.show(event.data);
+  start: function() {
+    var url = "ws://" + host + "/sp2tx";
+    sp2tx.socket = new WebSocket(url);
+    sp2tx.socket.onmessage = function (event) {
+      var text = event.data;
+      //console.log("/sp2tx recived: "+text);
+      if (text !== "") {
+        if (text!= "You"){
+          console.log("/sp2tx received: "+text);
         }
-    },
-    show: function (sp2tx) {
-        console.log(sp2tx)
+        if (text === "Surprise Me") {
+          surpriseMe();
+        } else if (text === "Guided breathing.") {
+          window.location.href = "breathing.html"; // Replace with your actual file name
+        } else if (text === "Mindfulness activity.") {
+          window.location.href = "mindfulness.html"; // Replace with your actual file name
+        } else if (text === "Stretching") {
+          window.location.href = "stretching.html";
+        } else {
+        
+        }
+      }
     }
+  }
+};
+
+
+function surpriseMe() {
+  const randomNumber = Math.random(); // Generate a random number between 0 and 1
+
+  if (randomNumber <= 1/3) {
+    window.location.href = "breathing.html"; // Replace with your actual file name
+  } else if (randomNumber <= 2/3) {
+    window.location.href = "mindfulness.html"; // Replace with your actual file name
+  } else {
+    window.location.href = "stretching.html"; // Replace with your actual file name
+  }
 }
